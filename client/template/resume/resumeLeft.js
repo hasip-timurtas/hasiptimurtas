754e58959472462bf0666981abcba8cb3f308db8
hasip timurtaş <hasip timurta>
2016-01-07 01:31:32 +0200

    arranged some accidents

--- client/template/resume/resumeLeft.js
+++ client/template/resume/resumeLeft.js
@@ -2,6 +2,14 @@
 Template.resumeLeft.events({
 	"click .certificate":function(event) {
         ShowBootstrapModel(event.currentTarget);
+	},
+
+	"click .resume-pic": function(event) {
+	    $(event.currentTarget).attr('data-toggle', 'modal');
+		$(event.currentTarget).attr('data-target', '#myModal');
+		var imageSrc = '/img/' + $(event.currentTarget).attr('data-src');
+		$(".modal-body img").attr('src',imageSrc);
+
 	}
 });
 
